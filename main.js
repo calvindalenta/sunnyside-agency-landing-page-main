1f296ace08097b2bdad6299fafd83efa3d18336e
Calvin Dalenta <calvindalentark@gmail.com>
2021-09-24 23:22:55 +0700

    Add hamburger click event and window on-resize event

---
+++ main.js
@@ -0,0 +1,23 @@
+const hamburger = document.querySelector('.hamburger');
+const nav = document.querySelector('.top nav');
+let isNavOpen = false;
+
+hamburger.addEventListener('click', () => {
+    if (isNavOpen) {
+        isNavOpen = false;
+        nav.style.display = 'none';
+    } else {
+        isNavOpen = true;
+        nav.style.display = 'block';
+    }
+});
+
+const breakpoint = 1024;
+window.onresize = () => {
+    console.log(window.innerWidth)
+    if (window.innerWidth > breakpoint) {
+        nav.style.display = 'block'
+    } else {
+        nav.style.display = 'none';
+    }
+};
